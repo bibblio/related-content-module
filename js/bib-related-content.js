d69059213affea912f1bab70a564ef47738734fd
Nic Young <nic.youngster@gmail.com>
2016-03-10 14:43:11 +0000

    Removed duplicate related-by, and no longer applying title case to them either

--- js/bib-related-content.js
+++ js/bib-related-content.js
@@ -52,9 +52,7 @@ function bib_displayRelatedContent(relatedContentItems, contentItemTemplate) {
 
 function bib_renderContentItemTemplate(contentItem, contentItemIndex, contentItemTemplate) {
     var compiled = _.template(contentItemTemplate);
-    var relatedBy = _.map(contentItem.relationships.inCommon.slice(0, 3), function (rel) {
-        return bib_toTitleCase(rel.text);
-    });
+    var relatedBy = bib_getRelatedBy(contentItem);
     var varBindings = {
         name: bib_toTitleCase(contentItem.fields.name),
         url: contentItem.fields.url,
@@ -63,6 +61,14 @@ function bib_renderContentItemTemplate(contentItem, contentItemIndex, contentIte
         rowNumber: (contentItemIndex + 1)
     };
     return compiled(varBindings);
+}
+
+function bib_getRelatedBy(contentItem) {
+  var relatedByTerms = _.map(contentItem.relationships.inCommon, function (rel) {
+        return rel.text;
+    });
+
+  return _.uniq(relatedByTerms).slice(0, 3);
 }
 
 function bib_recommendationUrl(contentItemId, limit, page, fields) {
